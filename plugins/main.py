38e8271cd6695ec4c7f7e84127380b8fd7bb73fb
HostyRockz <76691053+HostyRockz@users.noreply.github.com>
2021-03-24 14:27:46 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -3,9 +3,9 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery, InputTextMessageContent
 
 
-@bot.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start(client, message):
     chatid = message.chat.id
-    name = await bot.ask(chatid, 'YOUR NAME')
+    name = await Client.ask(chatid, 'YOUR NAME')
     got = name.text
-    await bot.send_message(chat_id=message.chat.id, text=f"{got}")
+    await Client.send_message(chat_id=message.chat.id, text=f"{got}")
