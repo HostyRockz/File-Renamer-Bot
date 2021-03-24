b437d04e105dd0b5d9d87dcbcbc7f44e22a41c6c
HostyRockz <76691053+HostyRockz@users.noreply.github.com>
2021-03-24 15:37:19 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -11,6 +11,6 @@ from translation import Translation
 @Client.on_message(filters.command("start"))
 async def start(client, message):
     chatid = message.chat.id
-    name = await bot.ask(chatid, 'YOUR NAME')
+    name = await client.ask(chatid, 'YOUR NAME')
     got = name.text
-    await bot.send_message(chat_id=message.chat.id, text=f"{got}")
+    await client.send_message(chat_id=message.chat.id, text=f"{got}")
