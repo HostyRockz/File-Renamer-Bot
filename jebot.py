f3e8c0be02d4a1be093b21d7566299cd23bcb7af
HostyRockz <76691053+HostyRockz@users.noreply.github.com>
2021-03-24 14:36:16 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -1,25 +1,13 @@
 import logging
 from pyrogram import Client
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import os
-import time
-
-# the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from sample_config import Config
+from Config import Config
 
 logging.basicConfig(level=logging.INFO)
 
 plugins = dict(
     root="plugins",
     include=[
-        "forceSubscribe",
-        "help"
+        "main"
     ]
 )
 
