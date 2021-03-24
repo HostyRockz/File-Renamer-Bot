924674be8cb147810db96709be7a0289748419b4
HostyRockz <76691053+HostyRockz@users.noreply.github.com>
2021-03-24 15:03:45 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -9,6 +9,6 @@ from pyrogram import Client, filters
 from translation import Translation
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery, InputTextMessageContent
 
-@Client.on_message(filters.command["start"])
+@Client.on_message(filters.command("start"))
 async def start(client, message):
   await bot.send_message(chat_id=message.chat.id, text=f"YO")
