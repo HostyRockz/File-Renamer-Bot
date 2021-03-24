06631a49e9dfa9377f6574b90f54c03bc789b2b4
HostyRockz <76691053+HostyRockz@users.noreply.github.com>
2021-03-24 14:41:51 +0530

    Update main.py

--- plugins/main.py
+++ plugins/main.py
@@ -3,14 +3,6 @@ from pyromod import listen
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery, InputTextMessageContent
 
-
-import logging
-from Config import Messages as tr
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-logging.basicConfig(level=logging.INFO)
-
 @Client.on_message(filters.private & filters.incoming & filters.command(['start']))
 def _start(client, message):
     client.send_message(message.chat.id,
